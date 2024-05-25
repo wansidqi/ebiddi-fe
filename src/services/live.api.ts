d3cf74d561d4192264d122d948f8440af7558455
wansidqi <wanahmadsidqi@gmail.com>
2024-05-25 08:01:26 +0800

    Update live.api.ts

--- src/services/live.api.ts
+++ src/services/live.api.ts
@@ -5,13 +5,6 @@ import { AuctionLiveItem } from "@/interfaces";
 import { useStoreContext } from "@/Context";
 import { CreditResponse } from "@/interfaces/API";
 
-// https://api.e-biddi.com/api/events/1144/minimal {minimal} GET {1144 = eventId} get short detail about event
-
-// https://api.e-biddi.com/api/profile/232/auctionhouse/2/credits {credits} GET
-
-// https://api.e-biddi.com/api/auction/68847 {id} GET {68847 = item?}
-// 68843: bidder detail
-
 const useGetCredit = (auctionHouseId: string) => {
   const { USER } = useStoreContext();
   return useQuery({
