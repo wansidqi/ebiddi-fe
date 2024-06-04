c239c5a9bed8c17b024c04f5182d2b342d0cd679
wansidqi <wanahmadsidqi@gmail.com>
2024-06-04 19:00:47 +0800

    sub and pub initialize

--- src/Context/store/websocket-context.tsx
+++ src/Context/store/websocket-context.tsx
@@ -1,10 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import * as socketClusterClient from "socketcluster-client";
 
-const AppContext = createContext<Data | null>(null);
-
-type ws = socketClusterClient.AGClientSocket | null;
-
 export function useWsContext() {
   const context = useContext(AppContext);
   if (!context) {
@@ -13,38 +9,69 @@ export function useWsContext() {
   return context;
 }
 
+const AppContext = createContext<Data | null>(null);
+
+type ws = socketClusterClient.AGClientSocket | null;
+
+interface SubscribeParams<T> {
+  id: string;
+  channel: "event" | "bid" | "status";
+  onData: (data: T) => any;
+}
+
 type Data = {
   socket: ws;
-  setSocket: React.Dispatch<React.SetStateAction<ws>>;
+  publishData<T>(name: string, data: T): void;
+  subscription<T>(params: SubscribeParams<T>): Promise<void>;
+  testSubscription<T>(name: string, onData: (data: T) => void): Promise<void>;
+
 };
 
 function WsContext(props: React.PropsWithChildren<{}>) {
   const [socket, setSocket] = useState<ws>(null);
-  const contextValue: Data = {
-    socket,
-    setSocket,
-  };
 
-  const init = () => {
-    const socket = socketClusterClient.create({
-      hostname: "bidding.e-biddi.com",
-      secure: true,
-      port: 443,
-      autoConnect: false,
-      protocolVersion: 1,
-      path: "/socketcluster/",
-    });
+  async function testSubscription<T>(name: string, onData: (data: T) => void) {
+    if (!socket) return;
+    let channel = socket.subscribe(name);
 
-    try {
-      socket.connect();
-      setSocket(socket);
-    } catch (error) {
-      socket.disconnect();
-      console.error("Error connecting to WebSocket:", error);
+    for await (let data of channel) {
+      onData(data);
     }
-  };
+  }
+
+  async function subscription<T>(params: SubscribeParams<T>) {
+    if (!socket) return;
+
+    const topic =
+      params.channel === "event"
+        ? `/event/${params.id}`
+        : `/event/${params.id}/${params.channel}`;
+    const channel = socket.subscribe(topic);
+
+    for await (const data of channel) {
+      params.onData(data);
+    }
+  }
+
+  function publishData<T>(channelName: string, data: T) {
+    if (!socket) return;
+    socket.invokePublish(channelName, data);
+  }
 
   useEffect(() => {
+    const init = () => {
+      const newSocket = socketClusterClient.create({
+        hostname: "bidding.e-biddi.com",
+        secure: true,
+        port: 443,
+        autoConnect: true,
+        protocolVersion: 1,
+        path: "/socketcluster/",
+      });
+
+      newSocket.connect();
+      setSocket(newSocket);
+    };
     init();
     return () => {
       if (socket) {
@@ -52,6 +79,13 @@ function WsContext(props: React.PropsWithChildren<{}>) {
       }
     };
   }, []);
+
+  const contextValue: Data = {
+    socket,
+    testSubscription,
+    publishData,
+    subscription,
+  };
 
   return <AppContext.Provider value={contextValue} {...props} />;
 }
--- src/components/WsComponent.tsx
+++ src/components/WsComponent.tsx
@@ -1,11 +1,27 @@
 import { useStoreContext } from "@/Context";
+import { useEffect } from "react";
 
 export const WS = () => {
-  const { socket } = useStoreContext();
+  const { socket, testSubscription, publishData } = useStoreContext();
+
+  useEffect(() => {
+    testSubscription("TESTFROMSOCKET", (data) => {
+      console.log(data);
+    });
+    return () => {
+      console.log("close");
+      socket?.unsubscribe("TESTFROMSOCKET");
+    };
+  }, [socket]);
+
+  const sendData = async () => {
+    publishData("TESTFROMSOCKET", { message: "success publish data" });
+  };
 
   return (
     <div className="flexcenter-col min-h-screen gap-4">
       <span>The WebSocket is {socket ? socket.state : "disconnected"}</span>
+      <button onClick={sendData}>send data</button>
     </div>
   );
 };
