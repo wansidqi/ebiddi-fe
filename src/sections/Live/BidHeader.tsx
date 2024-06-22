d3e477509b987a085a3776bc22d627052eef8fbe
wansidqi <wanahmadsidqi@gmail.com>
2024-06-22 12:41:37 +0800

    Update BidHeader.tsx

--- src/sections/Live/BidHeader.tsx
+++ src/sections/Live/BidHeader.tsx
@@ -6,7 +6,7 @@ import { CreditInterface, EventsInterface } from "@/interfaces";
 import { BidStatus, ROLE } from "@/enum";
 import { numWithComma } from "@/lib/utils";
 import { KEY, useGetQueryData } from "@/services";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { playAudio } from "@/assets/audio";
 
@@ -15,6 +15,8 @@ export function BidHeader() {
 
   const { USER, bidStatus, setBidStatus, payload } = useStoreContext();
   const { countdown } = payload;
+
+  const [finalCall, setFinalCall] = useState(0);
 
   const queryKeyEvent = [KEY.credit, eventId];
   const event = useGetQueryData<EventsInterface>(queryKeyEvent);
@@ -52,6 +54,7 @@ export function BidHeader() {
   useEffect(() => {
     switch (countdown) {
       case 10:
+        setFinalCall(0);
         setBidStatus(2);
         break;
       case 8:
@@ -70,6 +73,7 @@ export function BidHeader() {
         break;
       case 0:
         if (payload.status !== "SOLD") {
+          setFinalCall(1);
           startAlert({
             call: "final call",
             variant: "final",
@@ -81,23 +85,28 @@ export function BidHeader() {
           setBidStatus(BidStatus.END);
         }
 
-        if (bidStatus === 2) {
-          /* run every 3 seconds */
-          setInterval(() => {
-            startAlert({
-              call: "final call",
-              variant: "final",
-              audioName: "call3",
-            });
-          }, 3000);
-        }
-
         break;
 
       default:
         break;
     }
   }, [countdown]);
+
+  useEffect(() => {
+    if (finalCall > 0 && finalCall <= 3) {
+      const interval = setInterval(() => {
+        setFinalCall((prevCount) => prevCount + 1);
+      }, 3000);
+
+      startAlert({
+        call: "final call",
+        variant: "final",
+        audioName: "call3",
+      });
+
+      return () => clearInterval(interval);
+    }
+  }, [finalCall]);
 
   return (
     <div>
